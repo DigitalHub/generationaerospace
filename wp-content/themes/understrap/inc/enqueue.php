738d54d53a491756e0ce699f1661bc226caa2020
Rachelle Yen <rachelle.quek@text100.com.my>
2017-12-28 01:04:04 +0800

    styling for base, menu and homepage

--- wp-content/themes/understrap/inc/enqueue.php
+++ wp-content/themes/understrap/inc/enqueue.php
@@ -12,10 +12,12 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 	function understrap_scripts() {
 		// Get the theme data.
 		$the_theme = wp_get_theme();
+		wp_enqueue_style( 'font-styles', 'https://fonts.googleapis.com/css?family=Barlow:100,400,600');
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.css', array(), $the_theme->get( 'Version' ), false );
 		wp_enqueue_script( 'jquery');
 		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), true);
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.js', array(), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/main.js', array(), $the_theme->get( 'Version' ), true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
---
+++ wp-content/themes/understrap/page-templates/navigation-top.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package basic
+ */
+
+?>
+<!-- <nav id="site-navigation" class="main-navigation" role="navigation"> 
+	<?php //wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+</nav>--><!-- #site-navigation -->
+<menu id="menu-content" class="menu-content">
+	<a href="#" class="menubuttonclose closehere">
+		<div class="headerbutt close_butt">
+			<i class="fal"></i>
+			<span>Close</span>
+		</div>
+	</a>
+	<div class="container">
+		<div class="menu__wrapper">
+			<input type="text" />
+			<div class="menu-content__wrapper">
+				<nav class="menu-content__nav">
+					<div class="menu-content__nav--label">menu</div>
+					<div class="menu-content__nav--login small-text"><a href="#">Login</a> or <a href="#">Register</a> to join us</div>
+					<div class="corner corner-top-left"></div>
+					<div class="corner corner-top-right"></div>
+					<div class="corner corner-bottom-left"></div>
+					<div class="corner corner-bottom-right"></div>
+					
+					<div class="menu-content__list">
+						<a href="#"><span>01</span>home</a>
+						<a href="#"><span>02</span>find a video</a>
+						<a href="#"><span>03</span>find an experiments</a>
+						<a href="#"><span>04</span>genaero trailblazers</a>
+						<a href="#"><span>05</span>events</a>
+						<a href="#"><span>06</span>the vault</a>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<div class="menu-content__footer small-text"><a href="#">Terms &amp; Conditions</a> | <a href="#">Privacy Policy</a></div>
+</menu>
