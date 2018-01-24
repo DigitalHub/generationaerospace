fe66599503ea52eff1481700900bc305a1818077
Rachelle Yen <rachelle.quek@text100.com.my>
2018-01-24 09:43:00 +0800

    removing landscape in mobile

--- wp-content/themes/understrap/page-templates/fullwidthpage.php
+++ wp-content/themes/understrap/page-templates/fullwidthpage.php
@@ -12,29 +12,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 
- <section class="subpage--hud">
- 	<?php $bgimg = get_template_directory_uri() . "/img/img-header_The-vault.jpg";
- 	include(locate_template('loop-templates/hub-base.php')); ?>
- 	<div class="subpage--content">
- 		<div class="container">
- 			<div class="col-xl-8 offset-xl-2 col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-xs-12">
- 				<h1><?php the_title(); ?></h1>
- 				<div class="text-center highlight">
- 					<?php while (have_posts()) : the_post();  the_content(); endwhile;?>
- 				</div>
- 			</div>
- 		</div>
- 	</div>
- </section>
+<section class="subpage--hud">
+	<?php $bgimg = get_template_directory_uri() . "/img/img-header_The-vault.jpg";
+	include(locate_template('loop-templates/hub-base.php')); ?>
+	<div class="subpage--content">
+		<div class="container">
+			<div class="col-xl-8 offset-xl-2 col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-xs-12">
+				<h1><?php the_title(); ?></h1>
+			</div>
+		</div>
+	</div>
+</section>
 <section class="sub_main">
 	<div class="wrapper" id="full-width-page-wrapper">
 		<div class="<?php echo esc_attr( $container ); ?>" id="content">
 			<div class="row">
 				<div class="col-md-12 content-area" id="primary">
 					<main class="site-main" id="main" role="main">
-						<?php while ( have_posts() ) : the_post(); ?>
-							<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-						<?php endwhile; // end of the loop. ?>
+						<?php while (have_posts()) : the_post();  the_content(); endwhile;?>
 					</main><!-- #main -->
 				</div><!-- #primary -->
 			</div><!-- .row end -->
