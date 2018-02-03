09c264916e013be6267b1f3830a38b39f426c223
Rachelle Yen <rachelle.quek@text100.com.my>
2018-02-03 17:59:21 +0800

    minor edits for dashboard

--- wp-content/themes/understrap/page-templates/members/video-row.php
+++ wp-content/themes/understrap/page-templates/members/video-row.php
@@ -22,24 +22,29 @@ $thumbnail_url = 'http://img.youtube.com/vi/'.$youtube_id.'/mqdefault.jpg';
 ?>
 
 <div class="row videopost--row">
-	<div class="col-lg-3">
+	<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
 		<a data-fancybox href="<?=$youtube?>" target="_blank">
 			<img src="<?=$thumbnail_url?>" alt="<?=$title?>">
 		</a>
 	</div>
-	<div class="col-lg-7">
+	<div class="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-7">
 		<p class="highlight">
 			<?php echo $title;
 			if($approved === 'pending' || $approved === 'draft') { echo '<span>(Pending Approval)<span>'; } 
 			?>
 		</p>
-		<span><?=$desc?>...</span>
+		<?php if($desc) echo '<p class="d-md-block d-sm-none d-none">'.$desc.'...</p>';
+		?>
 	</div>
-	<div class="col-lg-1">
-		<a class="edit_videopost--btn" href="<?php echo get_permalink( get_page_by_path( 'edit-a-video' ) ) ?>?id=<?=$video_id?>"><i class="fal fa-pencil"></i></a>
+	<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-l">
+		<div class="row">
+			<a class="edit_videopost--btn" href="<?php echo get_permalink( get_page_by_path( 'edit-a-video' ) ) ?>?id=<?=$video_id?>"><i class="fal fa-pencil"></i></a>
+		</div>
 	</div>
-	<div class="col-lg-1">
-		<a href="#" class="delete-video edit_videopost--btn" data-video-id="<?=$video_id?>" data-link-id="<?=$link_id?>"><i class="fal fa-trash"></i></a>
+	<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-l">
+		<div class="row">
+			<a href="#" class="delete-video edit_videopost--btn" data-video-id="<?=$video_id?>" data-link-id="<?=$link_id?>"><i class="fal fa-trash"></i></a>
+		</div>
 	</div>
 </div>
 
