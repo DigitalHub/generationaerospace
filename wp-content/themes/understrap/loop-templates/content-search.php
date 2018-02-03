204365ce16ff7f07ce7d93d3230d502bd7bf9004
Stefanie C <stef.c85@gmail.com>
2018-02-03 17:17:22 +0800

    Search page: removed picture

--- wp-content/themes/understrap/loop-templates/content-search.php
+++ wp-content/themes/understrap/loop-templates/content-search.php
@@ -8,7 +8,7 @@
 ?>
 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-x-12 vault--card">
 	<a href="<?php the_permalink(); ?>">
-		<?php custom_post_thumbnail(); ?>
+		<?php //custom_post_thumbnail(); ?>
 
 		<div class="vault_card--content">
 			<?php the_title(); ?>
